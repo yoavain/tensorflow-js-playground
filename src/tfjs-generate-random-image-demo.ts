8d79c8f581f1fb1b80b9b37f45031075d702136a
Yoav Vainrich <yoavain@gmail.com>
2020-07-06 23:08:38 +0300

    TensorFlow JS * Random image

--- src/tfjs-generate-random-image-demo.ts
+++ src/tfjs-generate-random-image-demo.ts
@@ -1,21 +1,10 @@
-import * as tf from "@tensorflow/tfjs-node-gpu";
-import { Rank, Tensor3D, TensorBuffer } from "@tensorflow/tfjs-node-gpu";
+import { randomNormal, Tensor3D } from "@tensorflow/tfjs-node-gpu";
 import { encodeImage } from "./tfjs-image-utils";
 
 const main = async (dimension: number) => {
-    let inputShape: [number, number, number] = [dimension, dimension, 3];
-    let tensorBuffer: TensorBuffer<Rank, "int32"> = tf.buffer(inputShape, "int32");
-
-    for (let x = 0; x < dimension; x++) {
-        for (let y = 0; y < dimension; y++) {
-            for (let c = 0; c < 3; c++) {
-                tensorBuffer.set(x, y, c, Math.floor(Math.random() * 256 * 256 * 256));
-            }
-        }
-    }
-
-    let r3Tensor: Tensor3D = tf.tensor3d(new Uint8Array(tensorBuffer.values), inputShape);
-    await encodeImage(r3Tensor, "./resources/image.png", "random");
+    const inputShape: [number, number, number] = [dimension, dimension, 3];
+    let randomTensor: Tensor3D = randomNormal(inputShape, 128, 50, "int32");
+    await encodeImage(randomTensor, "./resources/image.png", "random");
 };
 
 // run
